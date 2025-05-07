b61a7d5d402e745e1f51e250d2178c02ba424f72
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-05-07 13:29:40 -0600

    Movie app

---
+++ mobile_movie_app/app/index.tsx
@@ -0,0 +1,12 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="items-center justify-center flex-1">
+      <Text className="text-5xl text-blue-500">
+        Welcome Sunny. How are you fine?
+      </Text>
+    </View>
+  );
+}
